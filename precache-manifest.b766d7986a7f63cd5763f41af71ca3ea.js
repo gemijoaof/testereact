f6cbba13af9a5b66b009ea4a13afc744948bb383
gemijoaof <gemijoaof@gmail.com>
2019-12-20 16:58:07 +0000

    Updates

---
+++ precache-manifest.b766d7986a7f63cd5763f41af71ca3ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987105426481a8a8486ff0ea6e265fae",
+    "url": "/gemijoaof/testereact/index.html"
+  },
+  {
+    "revision": "65dacf7d069e69789081",
+    "url": "/gemijoaof/testereact/static/css/main.4d275c77.chunk.css"
+  },
+  {
+    "revision": "f2e8ddf64e6728df8774",
+    "url": "/gemijoaof/testereact/static/js/2.64a586f9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/gemijoaof/testereact/static/js/2.64a586f9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "65dacf7d069e69789081",
+    "url": "/gemijoaof/testereact/static/js/main.2f38b1dd.chunk.js"
+  },
+  {
+    "revision": "06d51affe3d5b8d7d08c",
+    "url": "/gemijoaof/testereact/static/js/runtime-main.00fe786d.js"
+  }
+]);
